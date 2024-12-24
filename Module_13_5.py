dee5fee023096af310ee04ea70f436e0a5bcdcaa
robmar81 <mra-21@yandex.ru>
2024-12-24 22:18:53 +0300

    Update Module_13_5.py

--- Module_13_5.py
+++ Module_13_5.py
@@ -55,6 +55,10 @@ async def send_calories(message, state):
 async def start_message(message):
     await message.answer("Привет! Я бот помогающий твоему здоровью", reply_markup=kb)
 
+@dp.message_handler()
+async def all_message(message):
+    await message.answer('Введите команду /start, чтобы начать общение.')
+
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
 
